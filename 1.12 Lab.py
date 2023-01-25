9f4895d004545f8dc52236103f95586f5e7dc3d2
rmrahman430 <123211342+rmrahman430@users.noreply.github.com>
2023-01-24 22:42:36 -0600

    Delete 1.12 Lab.py

--- 1.12 Lab.py
+++
@@ -1,5 +0,0 @@
-#Rayyan Rahman ID: 1893113
-userNum = int(input())
-userNumSquared = userNum ** 2 # Bug here; fix it when instructed
-
-print(userNumSquared) # Output formatting issue here; fix it when instructed
