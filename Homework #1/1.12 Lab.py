fda4566000d20d527037281b37901f4e8561b013
rmrahman430 <123211342+rmrahman430@users.noreply.github.com>
2023-01-24 22:43:28 -0600

    Add files via upload

---
+++ Homework #1/1.12 Lab.py
@@ -0,0 +1,5 @@
+#Rayyan Rahman ID: 1893113
+userNum = int(input())
+userNumSquared = userNum ** 2 # Bug here; fix it when instructed
+
+print(userNumSquared) # Output formatting issue here; fix it when instructed
---
+++ Homework #1/1.20 Lab.py
@@ -0,0 +1,14 @@
+#Rayyan Rahman ID: 1893113
+userNum = int(input("Enter integer:\n"))
+
+print('You entered:', userNum)
+
+userNumSquared = userNum ** 2
+print(userNum, "squared is", userNumSquared)
+
+print('And', userNum,'cubed is', userNum * userNum * userNum, '!!')
+
+userNumTwo = int(input("Enter another integer:\n"))
+
+print(userNum, "+", userNumTwo, "is", userNum + userNumTwo)
+print(userNum, "*", userNumTwo, "is", userNum * userNumTwo)
